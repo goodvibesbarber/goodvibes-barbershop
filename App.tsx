b4a6483b1e6b8261a8dfc942e4faf0a1efaad360
goodvibesbarber <sanaeelghazi007@gmail.com>
2026-02-10 11:16:04 -0800

    Add sendNotification import to App.tsx
    Added import statement for sendNotification from notifier.ts.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+     import React, { useState } from 'react';
 // ... your other imports ...
 import { sendNotification } from './notifier.ts'; // <--- ADD THIS
 
